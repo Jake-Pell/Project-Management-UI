71b91619fa9769d67d9501b453de15aa0d964421
Jake-Pell <jakepell35@gmail.com>
2023-12-05 14:22:48 -0500

    working on #89

--- projectmanagement/src/main/java/controllers/LoginController.java
+++ projectmanagement/src/main/java/controllers/LoginController.java
@@ -2,12 +2,25 @@ package controllers;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import library.App;
+import model.ProjectApplication;
 
 public class LoginController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("signup");
+    private TextField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private void switchToProjectList() throws IOException {
+        ProjectApplication pa = ProjectApplication.getInstance();
+        String pword = password.getText();
+        String uname = username.getText();
+        if (pa.login(uname, pword))
+            App.setRoot("projectList");
     }
 }
