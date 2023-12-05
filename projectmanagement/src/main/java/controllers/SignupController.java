cdfcbb5196ffc480a7ff9c9f30c34c0aa4c10f9a
Arshia Eslami <arshia.esl83@gmail.com>
2023-12-05 14:22:20 -0500

    Adding new Files

--- projectmanagement/src/main/java/controllers/SignupController.java
+++ projectmanagement/src/main/java/controllers/SignupController.java
@@ -1,13 +1,31 @@
 package controllers;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import library.App;
 
 public class SignupController {
 
+
+    @FXML
+    private TextField txtsignup;
+
+
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("login");
+    }
+
+    @FXML
+    private void signupButton() throws IOException{
+        System.out.println(txtsignup.getText());
+        App.setRoot("project");
+    }
+
+    @FXML
+    private void onClick() throws IOException{
+
     }
 }
