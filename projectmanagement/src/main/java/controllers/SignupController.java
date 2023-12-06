55428c7fcc3be739134ebfc70198d4a3c4748f01
Jake-Pell <jakepell35@gmail.com>
2023-12-06 00:21:57 -0500

    working on #89 and #88

--- projectmanagement/src/main/java/controllers/SignupController.java
+++ projectmanagement/src/main/java/controllers/SignupController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import library.App;
 import model.ProjectApplication;
 
@@ -22,6 +24,13 @@ public class SignupController {
     @FXML
     private TextField username;
 
+    @FXML
+    private Text LoginSwitch;
+
+    @FXML
+    void Login(MouseEvent event) throws IOException {
+        App.setRoot("login");
+    }
 
     @FXML
     private void signupButton(ActionEvent event ) throws IOException{
@@ -38,11 +47,6 @@ public class SignupController {
         } else {
             App.setRoot("signup");
         }
-    }
-
-    @FXML
-    private void loginSwitch() throws IOException{
-        App.setRoot("login");
     }
 
 }
