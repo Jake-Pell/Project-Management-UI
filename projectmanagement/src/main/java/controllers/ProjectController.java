46f8288932643b5c510b248464c9b16424886ccb
Arshia Eslami <arshia.esl83@gmail.com>
2023-12-06 22:30:59 -0500

    adding home button

--- projectmanagement/src/main/java/controllers/ProjectController.java
+++ projectmanagement/src/main/java/controllers/ProjectController.java
@@ -1,5 +1,18 @@
 package controllers;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import library.App;
+
 public class ProjectController {
-    
+   
+    @FXML
+    private Button home;
+
+    @FXML
+    private void getProjectList() throws IOException {
+        App.setRoot("projectList");
+    }
 }
