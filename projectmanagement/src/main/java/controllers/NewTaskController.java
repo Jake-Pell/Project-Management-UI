af7e71cb795807e973387635c06aeee6337fb3e5
Jake-Pell <jakepell35@gmail.com>
2023-12-07 15:52:09 -0500

    working on #93

--- projectmanagement/src/main/java/controllers/NewTaskController.java
+++ projectmanagement/src/main/java/controllers/NewTaskController.java
@@ -1,11 +1,14 @@
 package controllers;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import library.App;
 
 public class NewTaskController {
 
@@ -13,7 +16,7 @@ public class NewTaskController {
     private Button createTaskButton;
 
     @FXML
-    private TextArea disrpitoms;
+    private TextArea description;
 
     @FXML
     private Button exit;
@@ -22,8 +25,11 @@ public class NewTaskController {
     private TextField taskName;
 
     @FXML
-    void projectList(ActionEvent event) {
+    private ComboBox priorityBox;
 
+    @FXML
+    private void backToProjectList(ActionEvent event) throws IOException {
+        App.setRoot("project");
     }
 
     @FXML
