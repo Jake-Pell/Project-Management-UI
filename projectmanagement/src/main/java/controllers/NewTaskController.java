e6330bfcfe124cc853f93003eb8626e32185209c
Jake-Pell <jakepell35@gmail.com>
2023-12-07 23:33:58 -0500

    working on #93

--- projectmanagement/src/main/java/controllers/NewTaskController.java
+++ projectmanagement/src/main/java/controllers/NewTaskController.java
@@ -1,6 +1,11 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,8 +14,9 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import library.App;
+import model.ProjectApplication;
 
-public class NewTaskController {
+public class NewTaskController implements javafx.fxml.Initializable {
 
     @FXML
     private Button createTaskButton;
@@ -25,7 +31,10 @@ public class NewTaskController {
     private TextField taskName;
 
     @FXML
-    private ComboBox priorityBox;
+    private ComboBox<Integer> priorityBox;
+
+    @FXML
+    private ComboBox<String> columnSelectBox;
 
     @FXML
     private void backToProjectList(ActionEvent event) throws IOException {
@@ -35,6 +44,14 @@ public class NewTaskController {
     @FXML
     void taskList(MouseEvent event) {
 
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        ProjectApplication pa = ProjectApplication.getInstance();
+        ArrayList<String> columnOptions = pa.getProjectColumns();
+        columnSelectBox.setItems(FXCollections
+                                 .observableArrayList(columnOptions));
     }
 
 }
